87a80c22bae81287b82f74ce3c9e99de711b0059
Ansel <anselayala13@gmail.com>
2018-08-02 22:01:09 -0700

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -5,7 +5,7 @@
  """
 
 Config = {
-    "password" : "generalpassword for each account created ",
-    "firebase_url" : "database url",
-    "proxy_server" : "input proxy_server" #213.168.210.76 sample proxy server to not let instagram block your ip
+    "password" : "SecretPassWord###,
+    "firebase_url" : "https://console.firebase.google.com/u/0/project/test-dbbb9/database/firestore/data~2Fl",
+    "proxy_server" : "38.29.152.9:53281" #213.168.210.76 sample proxy server to not let instagram block your ip
 }
